b39bda62b33476df1afa99ec33dc22404787de22
dp <1534513+plopd@users.noreply.github.com>
2023-04-27 08:43:28 +0200

    Add utils

---
+++ utils/calculate_state_distribution_chain.py
@@ -0,0 +1,98 @@
+import sys
+
+import numpy as np
+from tqdm import tqdm
+
+import agents.agents as agents
+import environments.environments as envs
+from rl_glue.rl_glue import RLGlue
+
+
+def calculate_state_distribution(N):
+    agent_info = {
+        "num_states": N,
+        "algorithm": "TD",
+        "representations": "TA",
+        "discount_rate": 1,
+        "trace_decay": 0,
+        "step_size": 0.125,
+        "seed": 0,
+        "interest": "UI",
+    }
+
+    env_info = {"env": "RandomWalk", "num_states": N}
+
+    exp_info = {
+        "max_timesteps_episode": 1000000,
+        "episode_eval_freq": 1,
+        "n_episodes": 1,
+    }
+
+    rl_glue = RLGlue(
+        envs.get_environment(env_info["env"]), agents.get_agent(agent_info["algorithm"])
+    )
+
+    rl_glue.rl_init(agent_info, env_info)
+
+    eta = np.zeros(env_info["num_states"])
+    last_state, _ = rl_glue.rl_start()
+    for _ in tqdm(range(1, int(exp_info["max_timesteps_episode"]) + 1)):
+        eta[last_state] += 1
+        _, last_state, _, term = rl_glue.rl_step()
+        if term:
+            last_state, _ = rl_glue.rl_start()
+
+    state_distribution = eta / np.sum(eta)
+
+    return state_distribution
+
+
+N_STATES = 1000
+
+# all states
+STATES = np.arange(0, N_STATES)
+
+# start from a central state
+START_STATE = 500
+
+# terminal states
+END_STATES = [-1, N_STATES]
+
+# possible actions
+ACTION_LEFT = -1
+ACTION_RIGHT = 1
+ACTIONS = [ACTION_LEFT, ACTION_RIGHT]
+
+# maximum stride for an action
+STEP_RANGE = 100
+
+
+def compute_true_value():
+    # true state value, just a promising guess
+    true_value = np.arange(-1001, 1003, 2) / 1001.0
+
+    # Dynamic programming to find the true state values, based on the promising guess above
+    # Assume all rewards are 0, given that we have already given value -1 and 1 to terminal states
+    while True:
+        old_value = np.copy(true_value)
+        for state in STATES:
+            true_value[state] = 0
+            for action in ACTIONS:
+                for step in range(1, STEP_RANGE + 1):
+                    step *= action
+                    next_state = state + step
+                    next_state = max(min(next_state, N_STATES + 1), 0)
+                    # asynchronous update for faster convergence
+                    true_value[state] += 1.0 / (2 * STEP_RANGE) * true_value[next_state]
+        error = np.sum(np.abs(old_value - true_value))
+        if error < 1e-2:
+            break
+    # correct the state value for terminal states to 0
+    true_value[0] = true_value[-1] = 0
+
+    return true_value
+
+
+if __name__ == "__main__":
+    N = int(sys.argv[1])
+    print(calculate_state_distribution(N))
---
+++ utils/utils.py
@@ -0,0 +1,60 @@
+import logging
+import os
+
+import numpy as np
+
+
+def path_exists(path):
+    if not path.exists():
+        os.makedirs(path, exist_ok=True)
+
+    return path
+
+
+def normalize_to_unit(x):
+    return x / np.linalg.norm(x, axis=1).reshape((-1, 1))
+
+
+def emphasis_limit(interest, discount_rate, trace_decay):
+    return (interest - discount_rate * trace_decay * interest) / (1 - discount_rate)
+
+
+def minmax_normalization_ab(x, min_x, max_x, a, b):
+    x_norm = a + ((x - min_x) * (b - a)) / (max_x - min_x)
+
+    return x_norm
+
+
+def per_feature_step_size_fourier_KOT(step_size, num_features, C):
+    step_size = step_size * np.ones(num_features)
+    step_size[1:] /= np.sqrt(np.sum(np.square(C[1:, :]), axis=1))
+
+    return step_size
+
+
+def remove_keys_with_none_value(dct):
+    """
+    Remove any keys with value None
+    Returns:
+
+    """
+    filtered = {k: v for k, v in dct.items() if v is not None}
+    dct.clear()
+    dct.update(filtered)
+
+    return dct
+
+
+def get_simple_logger(module_name, output_filepath):
+    # create logger on the current module and set its level
+    logger = logging.getLogger(module_name)
+    logger.setLevel(logging.INFO)
+
+    logging.basicConfig(
+        filename=output_filepath,
+        filemode="w",
+        format="%(asctime)s-%(levelname)s-%(message)s",
+        datefmt="%d-%b-%y %H:%M:%S",
+    )
+
+    return logger
